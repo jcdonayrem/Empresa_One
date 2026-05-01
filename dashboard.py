adaae7e562e3b8784bb93b61ab4473a8e94edcf2
JoTa <jcdonayrem@gmail.com>
2026-05-01 17:48:53 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -14,7 +14,7 @@ st.markdown("---")
 # Cargar datos
 @st.cache_data
 def load_data():
-    df = pd.read_csv("/content/drive/MyDrive/Online Sales Data.csv") # Corrected file path
+    df = pd.read_csv("Online Sales Data.csv") # Corrected file path
     df['Date'] = pd.to_datetime(df['Date'])
     df['Month'] = df['Date'].dt.to_period('M').astype(str)
     df['Year'] = df['Date'].dt.year
